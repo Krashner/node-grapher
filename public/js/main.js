a908b01db3ef210aba14305032b07cbc60309d1b
Krashner <jbuttrey@oddrealms.com>
2021-07-12 12:37:33 -0500

    Added ability to delete lines

--- public/js/main.js
+++ public/js/main.js
@@ -28,25 +28,29 @@ let node = `<div class="node noselect" ontouchstart ="selectNode(this)" ontouche
                     <div class="item-text cell2">Add New Item</div>
                 </div>
                 <div class="connection-point connection-point-node" onmousedown="createLine(this)" onmouseenter="selectConnection(this)" onmouseleave="deselectConnection(this)"></div>
-            </div>`
+            </div>`;
 
 let nodeItem = `<div class="item">
                     <div class="connection-point connection-point-item cell1" onmousedown="createLine(this)" onmouseenter="selectConnection(this)" onmouseleave="deselectConnection(this)"></div>
                     <div class="item-text cell2">Item 1</div>
                     <div class="item-text cell3 item-delete" onclick="deleteItem(this)"><i class="fas fa-trash"></i></div>
                     <div class="connection-point connection-point-item cell4" onmousedown="createLine(this)" onmouseenter="selectConnection(this)" onmouseleave="deselectConnection(this)"></div>
-                </div>`
+                </div>`;
 
 //get the node graph and add event listeners
 let nodeGraph = document.getElementById("node-graph");
-if(nodeGraph){
-    nodeGraph.addEventListener("touchstart", dragStart, false);
-    nodeGraph.addEventListener("touchend", dragEnd, false);
-    nodeGraph.addEventListener("touchmove", drag, false);
+if (nodeGraph) {
+	nodeGraph.addEventListener("touchstart", dragStart, false);
+	nodeGraph.addEventListener("touchend", dragEnd, false);
+	nodeGraph.addEventListener("touchmove", drag, false);
 
-    nodeGraph.addEventListener("mousedown", dragStart, false);
-    nodeGraph.addEventListener("mouseup", dragEnd, false);
-    nodeGraph.addEventListener("mousemove", drag, false);
+	nodeGraph.addEventListener("mousedown", dragStart, false);
+	nodeGraph.addEventListener("mouseup", dragEnd, false);
+	nodeGraph.addEventListener("mousemove", drag, false);
+	nodeGraph.addEventListener("contextmenu", (e) => {
+		findLineIntersect(nodeGraph, e);
+		e.preventDefault();
+	});
 }
 
 //expand the menus
@@ -64,94 +68,99 @@ function expand(x) {
 
 //toggle the dialogue to add a node to graph
 function toggleNodeDialogue(x) {
-	x.parentNode.querySelector(".add-node-dialogue").classList.toggle("invisible");
+	x.parentNode
+		.querySelector(".add-node-dialogue")
+		.classList.toggle("invisible");
 }
 
 //toggle the dialogue to add a node to graph
 function hideNodeDialogue(x) {
-    x.parentNode.parentNode.classList.add("invisible");
-    document.getElementById("new-node-name").value = "";
+	x.parentNode.parentNode.classList.add("invisible");
+	document.getElementById("new-node-name").value = "";
 }
 
 //toggle the dialogue to add item to node
 function showItemDialogue(x) {
-    x.parentNode.querySelector(".add-item-dialogue").classList.remove("invisible");
-    x.classList.add("invisible");
-    updateLines(x.parentNode);
+	x.parentNode
+		.querySelector(".add-item-dialogue")
+		.classList.remove("invisible");
+	x.classList.add("invisible");
+	updateLines(x.parentNode);
 }
 
 //hide the dialogue controls
 function hideItemDialogue(x) {
-    x.parentNode.parentNode.classList.add("invisible");
-    x.parentNode.parentNode.parentNode.querySelector(".new-item").classList.remove("invisible");
-    x.parentNode.parentNode.querySelector(".add-item-input").value = "";
+	x.parentNode.parentNode.classList.add("invisible");
+	x.parentNode.parentNode.parentNode
+		.querySelector(".new-item")
+		.classList.remove("invisible");
+	x.parentNode.parentNode.querySelector(".add-item-input").value = "";
 }
 
 //started dragging node
 function dragStart(e) {
-    if (!activeItem && e.target === targetItem) {
-        active = true;
-        activeItem = targetItem;
-    
-        let xOffset = activeItem['xOffset'];
-        let yOffset = activeItem['yOffset'];
+	if (!activeItem && e.target === targetItem) {
+		active = true;
+		activeItem = targetItem;
 
-        if(!xOffset)
-            xOffset = 0;
-        if(!yOffset)
-            yOffset = 0;
+		let xOffset = activeItem["xOffset"];
+		let yOffset = activeItem["yOffset"];
 
-        if (e.type === "touchstart") {
-            initialX = e.touches[0].clientX - xOffset;
-            initialY = e.touches[0].clientY - yOffset;
-        } else {
-            initialX = e.clientX - xOffset;
-            initialY = e.clientY - yOffset;
-        }
-    }
+		if (!xOffset) xOffset = 0;
+		if (!yOffset) yOffset = 0;
+
+		if (e.type === "touchstart") {
+			initialX = e.touches[0].clientX - xOffset;
+			initialY = e.touches[0].clientY - yOffset;
+		} else {
+			initialX = e.clientX - xOffset;
+			initialY = e.clientY - yOffset;
+		}
+	}
 }
 
 //stopped dragging nodes and lines
 function dragEnd(e) {
-    if(!activeItem || !activeItem.nodeName)
-        return;
+	if (!activeItem || !activeItem.nodeName) return;
 
-    if(activeItem.nodeName == "DIV"){
-        activeItem = null;
-    }else if(targetConnection){
+	if (activeItem.nodeName == "DIV") {
+		activeItem = null;
+	} else if (targetConnection) {
+		//prevent starting and ending line at same point
+		if (
+			targetConnection["lines-start"] &&
+			targetConnection["lines-start"].includes(activeItem)
+		)
+			return;
 
-        //prevent starting and ending line at same point
-        if(targetConnection["lines-start"] && targetConnection["lines-start"].includes(activeItem))
-            return;
+		//connect the line to the connection point
+		let offsets = targetConnection.getBoundingClientRect();
+		let top = offsets.top + targetConnection.offsetHeight / 2;
+		let left = offsets.left + targetConnection.offsetWidth / 2;
 
-        //connect the line to the connection point
-        let offsets = targetConnection.getBoundingClientRect();
-        let top = offsets.top + targetConnection.offsetHeight / 2;
-        let left = offsets.left + targetConnection.offsetWidth / 2;
-    
-        for (let i = 0; i < activeItem.childNodes.length; i++) {
-            activeItem.childNodes[i].setAttribute("x2", left);
-            activeItem.childNodes[i].setAttribute("y2", top);
-        }
+		for (let i = 0; i < activeItem.childNodes.length; i++) {
+			activeItem.childNodes[i].setAttribute("x2", left);
+			activeItem.childNodes[i].setAttribute("y2", top);
+		}
 
-        if(!targetConnection["lines-end"])
-            targetConnection["lines-end"] = [];
-        targetConnection["lines-end"].push(activeItem);
+		if (!targetConnection["lines-end"]) targetConnection["lines-end"] = [];
+		targetConnection["lines-end"].push(activeItem);
+		activeItem["nodes"].push(targetConnection);
 
-        activeItem = null;
-    }else{
-        activeItem.parentNode.removeChild(activeItem);
-        activeItem = null;
-    }
-    // active = false;
-    initialX = currentX;
-    initialY = currentY;
+		activeItem = null;
+	} else {
+		activeItem.parentNode.removeChild(activeItem);
+		activeItem = null;
+	}
+	// active = false;
+	initialX = currentX;
+	initialY = currentY;
 }
 
 //drag the node or line around
 function drag(e) {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+	mouseX = e.clientX;
+	mouseY = e.clientY;
 
 	if (active && activeItem != null) {
 		e.preventDefault();
@@ -164,54 +173,51 @@ function drag(e) {
 			currentY = e.clientY - initialY;
 		}
 
-        activeItem['xOffset'] = currentX;
-        activeItem['yOffset'] = currentY;
+		activeItem["xOffset"] = currentX;
+		activeItem["yOffset"] = currentY;
 
-        if(activeItem.nodeName == "DIV"){
-            setPosition(currentX, currentY, activeItem);
-            updateLines(activeItem);
-        }else{
-            setEnd(e.clientX, e.clientY, activeItem);
-        }
-    }
+		if (activeItem.nodeName == "DIV") {
+			setPosition(currentX, currentY, activeItem);
+			updateLines(activeItem);
+		} else {
+			setEnd(e.clientX, e.clientY, activeItem);
+		}
+	}
 }
 
 //update all the lines connected to the moving node
 //TODO: Theres'a bug when the page size is changed. x2 & y2 are no longer accurate
-function updateLines(e){
-    let connectionPoints = e.querySelectorAll('.connection-point');
-    loopThroughLines(connectionPoints,"lines-start", "x1", "y1");
-    loopThroughLines(connectionPoints,"lines-end", "x2", "y2");
+function updateLines(e) {
+	let connectionPoints = e.querySelectorAll(".connection-point");
+	loopThroughLines(connectionPoints, "lines-start", "x1", "y1");
+	loopThroughLines(connectionPoints, "lines-end", "x2", "y2");
 }
 
 //loop through each connection point, svg, and line and update positions
-function loopThroughLines(arr, side, x, y){
+function loopThroughLines(arr, side, x, y) {
+	for (let n = 0; n < arr.length; n++) {
+		let svgArr = arr[n][side];
+		if (!svgArr) continue;
 
-    for (let n = 0; n < arr.length; n++) {
-        let svgArr = arr[n][side];
-        if(!svgArr)
-            continue;
+		let offsets = arr[n].getBoundingClientRect();
+		let top = offsets.top + arr[n].offsetHeight / 2 + nodeGraph.scrollTop;
+		let left = offsets.left + arr[n].offsetWidth / 2 + nodeGraph.scrollLeft;
 
-        let offsets = arr[n].getBoundingClientRect();
-        let top = offsets.top + arr[n].offsetHeight / 2  + nodeGraph.scrollTop;
-        let left = offsets.left + arr[n].offsetWidth / 2  + nodeGraph.scrollLeft;
-    
-        //update the line ending points
-        for (let g = 0; g < svgArr.length; g++) {
-            let svg = svgArr[g];
+		//update the line ending points
+		for (let g = 0; g < svgArr.length; g++) {
+			let svg = svgArr[g];
 
-            for (let i = 0; i < svg.childNodes.length; i++) {
-                svg.childNodes[i].setAttribute(x, left);
-                svg.childNodes[i].setAttribute(y, top);
-            }
-        }
+			for (let i = 0; i < svg.childNodes.length; i++) {
+				svg.childNodes[i].setAttribute(x, left);
+				svg.childNodes[i].setAttribute(y, top);
+			}
+		}
 	}
-
 }
 
 //move node
-function setPosition(xPos, yPos, e){
-    e.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
+function setPosition(xPos, yPos, e) {
+	e.style.transform = "translate3d(" + xPos + "px, " + yPos + "px, 0)";
 }
 
 //move line start position
@@ -222,97 +228,150 @@ function setStart(xPos, yPos, e) {
 	}
 }
 
-
 //move line end position
 function setEnd(xPos, yPos, e) {
 	for (let i = 0; i < e.childNodes.length; i++) {
-        if(xPos && yPos){
-            e.childNodes[i].setAttribute("x2", xPos);
-            e.childNodes[i].setAttribute("y2", yPos);
-        }
+		if (xPos && yPos) {
+			e.childNodes[i].setAttribute("x2", xPos);
+			e.childNodes[i].setAttribute("y2", yPos);
+		}
 	}
 }
 
 //select node when mouse enters element
-function selectNode(x){
-    targetItem = x;
+function selectNode(x) {
+	targetItem = x;
 }
 
 //deselect node when mouse enters element
-function deselectNode(x){
-    targetItem = null;
+function deselectNode(x) {
+	targetItem = null;
 }
 
 //select point when mouse enters element
-function selectConnection(x){
-    targetConnection = x;
+function selectConnection(x) {
+	targetConnection = x;
 }
 
 //deselect point when mouse enters element
-function deselectConnection(x){
-    targetConnection = null;
+function deselectConnection(x) {
+	targetConnection = null;
 }
 
 //delete the node, items and all connecting lines
-function deleteNode(x){
-    let connectionPoints = x.parentNode.parentNode.querySelectorAll('.connection-point');
-    deleteLines(connectionPoints);
-    let index = nodes.indexOf(x.parentNode.parentNode);
-    nodes = nodes.slice(index, 1);
-    x.parentNode.parentNode.parentNode.removeChild(x.parentNode.parentNode);
+function deleteNode(x) {
+	let connectionPoints =
+		x.parentNode.parentNode.querySelectorAll(".connection-point");
+	deleteLines(connectionPoints);
+	let index = nodes.indexOf(x.parentNode.parentNode);
+	nodes = nodes.slice(index, 1);
+	x.parentNode.parentNode.parentNode.removeChild(x.parentNode.parentNode);
 }
 
 //delete the clicked item and any lines connected to it
-function deleteItem(x){
+function deleteItem(x) {
+	let connectionPoints = x.parentNode.querySelectorAll(".connection-point");
+	deleteLines(connectionPoints);
+	let node = x.parentNode.parentNode.parentNode;
+	x.parentNode.parentNode.removeChild(x.parentNode);
+	if (node) updateLines(node);
+}
 
-    let connectionPoints = x.parentNode.querySelectorAll('.connection-point');
-    deleteLines(connectionPoints);
-    let node = x.parentNode.parentNode.parentNode;
-    x.parentNode.parentNode.removeChild(x.parentNode);
-    if(node)
-        updateLines(node);
+//Delete specific line from connected nodes
+function deleteLine(element) {
+	let nodes = element["nodes"];
 
+	for (let n = 0; n < nodes.length; n++) {
+
+        //remove lines from arrays
+        if(nodes[n]["lines-start"] != undefined && nodes[n]["lines-start"].indexOf(element) != -1)
+        {
+            let index = nodes[n]["lines-start"].indexOf(element);
+            nodes[n]["lines-start"].splice(index, 1);
+        }
+
+        if(nodes[n]["lines-end"] != undefined && nodes[n]["lines-end"].indexOf(element) != -1)
+        {
+            let index = nodes[n]["lines-end"].indexOf(element);
+            nodes[n]["lines-end"].splice(index, 1);
+        }
+	}
+
+	element.parentNode.removeChild(element);
 }
 
 //find all connecting lines and delete them
-function deleteLines(arr){
-    for (let n = 0; n < arr.length; n++) {
-        let svgArr = arr[n]["lines-start"];
-        if(!svgArr)
-            svgArr = [];
+function deleteLines(arr) {
+	for (let n = 0; n < arr.length; n++) {
+		let svgArr = arr[n]["lines-start"];
+		if (!svgArr) svgArr = [];
 
-        let svgArr2 = arr[n]["lines-end"];
-        if(svgArr2)
-            svgArr = svgArr.concat(svgArr2);
+		let svgArr2 = arr[n]["lines-end"];
+		if (svgArr2) svgArr = svgArr.concat(svgArr2);
 
-        if(svgArr.length===0)
-            continue;
+		if (svgArr.length === 0) continue;
 
-        for (let g = 0; g < svgArr.length; g++) {
-            let svg = svgArr[g];
-            svg.remove(svg); 
-        }
-    }
+		for (let g = 0; g < svgArr.length; g++) {
+			let svg = svgArr[g];
+			svg.remove(svg);
+		}
+	}
+}
+
+//find if and where the mouse intersects with a line, overrides contextmenu action
+function findLineIntersect(element, event) {
+	let svg = element.querySelectorAll(".lines");
+
+	if (svg.length > 0) {
+		svg.forEach((lineContainer) => {
+			let mousePos = getCursorPosition(element, event);
+			let line = lineContainer.firstChild;
+
+			let x1 = line.getAttribute("x1");
+			let y1 = line.getAttribute("y1");
+			let x2 = line.getAttribute("x2");
+			let y2 = line.getAttribute("y2");
+
+			let distAC = Math.sqrt(
+				Math.pow(x1 - mousePos.x, 2) + Math.pow(y1 - mousePos.y, 2)
+			);
+			let distCB = Math.sqrt(
+				Math.pow(mousePos.x - x2, 2) + Math.pow(mousePos.y - y2, 2)
+			);
+			let distAB = Math.sqrt(Math.pow(x1 - x2, 2) + Math.pow(y1 - y2, 2));
+
+			//if point is on the line +/- 1, then delete it
+			if (Math.abs(distAC + distCB - distAB) < 1) {
+				deleteLine(lineContainer);
+			}
+		});
+	}
+}
+
+function getCursorPosition(element, event) {
+	const rect = element.getBoundingClientRect();
+	const x = event.clientX - rect.left;
+	const y = event.clientY - rect.top;
+	//console.log("mouse: x: " + x + " y: " + y)
+	return { x, y };
 }
 
 //create a line starting at the clicked point
 function createLine(x) {
+	if (activeItem != null) return;
 
-    if(activeItem != null)
-        return;
-        
 	let offsets = x.getBoundingClientRect();
 	let top = offsets.top + x.offsetHeight / 2;
 	let left = offsets.left + x.offsetWidth / 2;
 
-    let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-    svg.style.position = "absolute";
-    svg.setAttribute("width", "100%");
-    svg.setAttribute("height", "100%");
-    svg.setAttribute("position", "absolute");
-    svg.setAttribute("pointer-events", "none");
-    svg.classList.add("lines");
-    
+	let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	svg.style.position = "absolute";
+	svg.setAttribute("width", "100%");
+	svg.setAttribute("height", "100%");
+	svg.setAttribute("position", "absolute");
+	svg.setAttribute("pointer-events", "none");
+	svg.classList.add("lines");
+
 	let line = document.createElementNS("http://www.w3.org/2000/svg", "line");
 	line.setAttribute("x1", left);
 	line.setAttribute("y1", top);
@@ -328,56 +387,63 @@ function createLine(x) {
 	line2.setAttribute("y2", mouseY);
 	line2.setAttribute("stroke", "white");
 	line2.setAttribute("stroke-width", "3");
-    
-	svg.appendChild(line);
-    svg.appendChild(line2);
 
-    active = true;
-    activeItem = svg;
-    
-    if(!x["lines-start"])
-        x["lines-start"] = [];
-    x["lines-start"].push(svg);
+	svg.appendChild(line);
+	svg.appendChild(line2);
+
+	active = true;
+	activeItem = svg;
+
+	if (!x["lines-start"]) x["lines-start"] = [];
+	x["lines-start"].push(svg);
+	if (!svg["nodes"]) svg["nodes"] = [];
+	svg["nodes"].push(x);
 
 	nodeGraph.appendChild(svg);
 }
 
 //create a new node
-function createNode(x) {    
-    let name = document.getElementById("new-node-name");
-    
-    if(name.value != ""){
-        document.querySelector(".add-node-dialogue").classList.toggle("invisible");
-        let newNode = document.createElement('div');
-        newNode.innerHTML = node.trim();
+function createNode(x) {
+	let name = document.getElementById("new-node-name");
 
-        let n = newNode.childNodes[0];
-        n.querySelector('.title-text').innerHTML = name.value;
-        
-        let offsets = n.getBoundingClientRect();
-        n['xOffset'] = offsets.left;
-        n['yOffset'] = offsets.top;
-        n['name'] = name.value;
-        nodes.push(n);
+	if (name.value != "") {
+		document
+			.querySelector(".add-node-dialogue")
+			.classList.toggle("invisible");
+		let newNode = document.createElement("div");
+		newNode.innerHTML = node.trim();
 
-        name.value = "";
-        nodeGraph.appendChild(n);
-        newNode.remove();
-    }
+		let n = newNode.childNodes[0];
+		n.querySelector(".title-text").innerHTML = name.value;
+
+		let offsets = n.getBoundingClientRect();
+		n["xOffset"] = offsets.left;
+		n["yOffset"] = offsets.top;
+		n["name"] = name.value;
+		nodes.push(n);
+
+		name.value = "";
+		nodeGraph.appendChild(n);
+		newNode.remove();
+	}
 }
 
 //create a new item
-function createItem(x) {    
-    let name = x.parentNode.parentNode.querySelector(".add-item-input");
-    if(name.value != ""){
-        x.parentNode.parentNode.classList.toggle("invisible");
-        x.parentNode.parentNode.parentNode.querySelector(".new-item").classList.toggle("invisible");
+function createItem(x) {
+	let name = x.parentNode.parentNode.querySelector(".add-item-input");
+	if (name.value != "") {
+		x.parentNode.parentNode.classList.toggle("invisible");
+		x.parentNode.parentNode.parentNode
+			.querySelector(".new-item")
+			.classList.toggle("invisible");
 
-        let item = document.createElement('div');
-        item.innerHTML = nodeItem.trim();
-        item.querySelector('.item-text').innerHTML = name.value;
-        name.value = "";
-        x.parentNode.parentNode.parentNode.querySelector('.item-list').innerHTML += item.innerHTML;
-        updateLines(x.parentNode.parentNode.parentNode);
-    }
+		let item = document.createElement("div");
+		item.innerHTML = nodeItem.trim();
+		item.querySelector(".item-text").innerHTML = name.value;
+		name.value = "";
+		x.parentNode.parentNode.parentNode.querySelector(
+			".item-list"
+		).innerHTML += item.innerHTML;
+		updateLines(x.parentNode.parentNode.parentNode);
+	}
 }
