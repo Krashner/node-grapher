23f929697afcad3928d3e0fb7a10ed4d05f44a5c
Krashner <jbuttrey@oddrealms.com>
2021-07-12 16:00:01 -0500

    fixed positioning for new nodes

--- public/js/main.js
+++ public/js/main.js
@@ -103,9 +103,9 @@ function dragStart(e) {
 		active = true;
 		activeItem = targetItem;
 
-        var style = window.getComputedStyle(activeItem);
-        var matrix = new WebKitCSSMatrix(style.transform);
-        
+		var style = window.getComputedStyle(activeItem);
+		var matrix = new WebKitCSSMatrix(style.transform);
+
 		if (e.type === "touchstart") {
 			initialX = e.touches[0].clientX - matrix.m41;
 			initialY = e.touches[0].clientY - matrix.m42;
@@ -113,7 +113,6 @@ function dragStart(e) {
 			initialX = e.clientX - matrix.m41;
 			initialY = e.clientY - matrix.m42;
 		}
-
 	}
 }
 
@@ -170,7 +169,7 @@ function drag(e) {
 			currentX = e.clientX - initialX;
 			currentY = e.clientY - initialY;
 		}
-        
+
 		if (activeItem.nodeName == "DIV") {
 			setPosition(currentX, currentY, activeItem);
 			updateLines(activeItem);
@@ -277,19 +276,22 @@ function deleteLine(element) {
 	let nodes = element["nodes"];
 
 	for (let n = 0; n < nodes.length; n++) {
+		//remove lines from arrays
+		if (
+			nodes[n]["lines-start"] != undefined &&
+			nodes[n]["lines-start"].indexOf(element) != -1
+		) {
+			let index = nodes[n]["lines-start"].indexOf(element);
+			nodes[n]["lines-start"].splice(index, 1);
+		}
 
-        //remove lines from arrays
-        if(nodes[n]["lines-start"] != undefined && nodes[n]["lines-start"].indexOf(element) != -1)
-        {
-            let index = nodes[n]["lines-start"].indexOf(element);
-            nodes[n]["lines-start"].splice(index, 1);
-        }
-
-        if(nodes[n]["lines-end"] != undefined && nodes[n]["lines-end"].indexOf(element) != -1)
-        {
-            let index = nodes[n]["lines-end"].indexOf(element);
-            nodes[n]["lines-end"].splice(index, 1);
-        }
+		if (
+			nodes[n]["lines-end"] != undefined &&
+			nodes[n]["lines-end"].indexOf(element) != -1
+		) {
+			let index = nodes[n]["lines-end"].indexOf(element);
+			nodes[n]["lines-end"].splice(index, 1);
+		}
 	}
 
 	element.parentNode.removeChild(element);
@@ -317,8 +319,8 @@ function deleteLines(arr) {
 function findLineIntersect(graph, event) {
 	let svg = graph.querySelectorAll(".lines");
 
-    //temp, need ui and auto save function
-    serializeNodes(graph);
+	//temp, need ui and auto save function
+	serializeNodes(graph);
 
 	if (svg.length > 0) {
 		svg.forEach((lineContainer) => {
@@ -347,13 +349,12 @@ function findLineIntersect(graph, event) {
 }
 
 //serialize node data
-function serializeNodes(graph){
-
-    //console.log(graph.innerHTML);
-    //console.log(JSON.stringify(graph.innerHTML));
-    //console.log( $( graph ).serialize() );
-    graph.innerHTML= "<div class=\"node noselect\" ontouchstart=\"selectNode(this)\" ontouchend=\"deselectNode(this)\" onmouseenter=\"selectNode(this)\" onmouseleave=\"deselectNode(this)\" style=\"transform: translate3d(551px, 169px, 0px);\">\n                <div class=\"connection-point connection-point-node\" ontouchstart=\"selectConnection(this)\" ontouchend=\"deselectConnection(this)\" onmousedown=\"createLine(this)\" onmouseenter=\"selectConnection(this)\" onmouseleave=\"deselectConnection(this)\"></div>\n                <div class=\"title\">\n                    <h4 class=\"title-text cell1\">sad</h4>\n                    <div class=\"cell2 node-delete\" onclick=\"deleteNode(this)\"><i class=\"fas fa-trash\"></i></div>\n                </div>\n                <div class=\"item-list\"><div class=\"item\">\n                    <div class=\"connection-point connection-point-item cell1\" onmousedown=\"createLine(this)\" onmouseenter=\"selectConnection(this)\" onmouseleave=\"deselectConnection(this)\"></div>\n                    <div class=\"item-text cell2\">asdsd</div>\n                    <div class=\"item-text cell3 item-delete\" onclick=\"deleteItem(this)\"><i class=\"fas fa-trash\"></i></div>\n                    <div class=\"connection-point connection-point-item cell4\" onmousedown=\"createLine(this)\" onmouseenter=\"selectConnection(this)\" onmouseleave=\"deselectConnection(this)\"></div>\n                </div></div>\n                <div class=\"add-item-dialogue invisible\">\n                    <input class=\"add-item-input\" type=\"text\" placeholder=\"Enter item...\" required=\"\">\n                    <div>      \n                    <button name=\"add-item-btn\" onclick=\"createItem(this)\">Add Item</button>\n                    <button onclick=\"hideItemDialogue(this)\">X</button>\n                    </div>\n                </div>\n                <div class=\"new-item noselect\" onclick=\"showItemDialogue(this)\">\n                    <div class=\"add-item cell1\">+</div>\n                    <div class=\"item-text cell2\">Add New Item</div>\n                </div>\n                <div class=\"connection-point connection-point-node\" onmousedown=\"createLine(this)\" onmouseenter=\"selectConnection(this)\" onmouseleave=\"deselectConnection(this)\"></div>\n            </div>"
-   
+function serializeNodes(graph) {
+	//console.log(graph.innerHTML);
+	//console.log(JSON.stringify(graph.innerHTML));
+	//console.log( $( graph ).serialize() );
+	graph.innerHTML =
+		'<div class="node noselect" ontouchstart="selectNode(this)" ontouchend="deselectNode(this)" onmouseenter="selectNode(this)" onmouseleave="deselectNode(this)" style="transform: translate3d(551px, 169px, 0px);">\n                <div class="connection-point connection-point-node" ontouchstart="selectConnection(this)" ontouchend="deselectConnection(this)" onmousedown="createLine(this)" onmouseenter="selectConnection(this)" onmouseleave="deselectConnection(this)"></div>\n                <div class="title">\n                    <h4 class="title-text cell1">sad</h4>\n                    <div class="cell2 node-delete" onclick="deleteNode(this)"><i class="fas fa-trash"></i></div>\n                </div>\n                <div class="item-list"><div class="item">\n                    <div class="connection-point connection-point-item cell1" onmousedown="createLine(this)" onmouseenter="selectConnection(this)" onmouseleave="deselectConnection(this)"></div>\n                    <div class="item-text cell2">asdsd</div>\n                    <div class="item-text cell3 item-delete" onclick="deleteItem(this)"><i class="fas fa-trash"></i></div>\n                    <div class="connection-point connection-point-item cell4" onmousedown="createLine(this)" onmouseenter="selectConnection(this)" onmouseleave="deselectConnection(this)"></div>\n                </div></div>\n                <div class="add-item-dialogue invisible">\n                    <input class="add-item-input" type="text" placeholder="Enter item..." required="">\n                    <div>      \n                    <button name="add-item-btn" onclick="createItem(this)">Add Item</button>\n                    <button onclick="hideItemDialogue(this)">X</button>\n                    </div>\n                </div>\n                <div class="new-item noselect" onclick="showItemDialogue(this)">\n                    <div class="add-item cell1">+</div>\n                    <div class="item-text cell2">Add New Item</div>\n                </div>\n                <div class="connection-point connection-point-node" onmousedown="createLine(this)" onmouseenter="selectConnection(this)" onmouseleave="deselectConnection(this)"></div>\n            </div>';
 }
 
 //returns the coordinates of the mouse
@@ -426,6 +427,9 @@ function createNode(x) {
 
 		n["name"] = name.value;
 		nodes.push(n);
+
+        //TODO:check if it's overlapping and offset by a few units if it is
+		setPosition(100, 100, n);
 
 		name.value = "";
 		nodeGraph.appendChild(n);
